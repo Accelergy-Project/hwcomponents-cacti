fcbfbfcc3d223f442cf48bf8ddf7d48b3bac616e
tanner-andrulis <andrulis@mit.edu>
2026-01-05 11:15:29 -0500

    Workflows check out submodules

--- setup_scm_version.py
+++ setup_scm_version.py
@@ -7,10 +7,10 @@ def post_version(version):
     if version.exact:
         return version.format_with("{tag}").lstrip('v')
 
-    base = str(version.tag).lstrip('v') if version.tag else (guess_next_version(version) or "1.0.0")
+    base = str(version.tag).lstrip('v') if version.tag else (guess_next_version(version) or "1.0")
     distance = version.distance or 0
 
-    return f"{base}.post{distance}" if distance > 0 else base
+    return f"{base}.{distance}" if distance > 0 else base
 
 
 def no_local(version):
